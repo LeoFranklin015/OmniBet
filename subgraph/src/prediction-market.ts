0360f57821929535adcaa526356cc6f09de90f98
LeoFranklin015 <leofranklinjohn.25cs@licet.ac.in>
2025-03-15 01:33:51 +0530

    feat: Subgraph done

--- subgraph/src/prediction-market.ts
+++ subgraph/src/prediction-market.ts
@@ -1,149 +1,128 @@
+import { BigInt } from "@graphprotocol/graph-ts";
 import {
   EmergencyLiquidityAdded as EmergencyLiquidityAddedEvent,
   LiquidityAdded as LiquidityAddedEvent,
   MarketCreated as MarketCreatedEvent,
   MarketResolved as MarketResolvedEvent,
+  MarketUpdated,
   MarketUpdated as MarketUpdatedEvent,
   OwnershipTransferred as OwnershipTransferredEvent,
   RewardClaimed as RewardClaimedEvent,
-  TokenOperation as TokenOperationEvent
-} from "../generated/PredictionMarket/PredictionMarket"
-import {
-  EmergencyLiquidityAdded,
-  LiquidityAdded,
-  MarketCreated,
-  MarketResolved,
-  MarketUpdated,
-  OwnershipTransferred,
-  RewardClaimed,
-  TokenOperation
-} from "../generated/schema"
+  TokenOperation as TokenOperationEvent,
+} from "../generated/PredictionMarket/PredictionMarket";
+import { Market } from "../generated/schema";
 
 export function handleEmergencyLiquidityAdded(
   event: EmergencyLiquidityAddedEvent
 ): void {
-  let entity = new EmergencyLiquidityAdded(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.owner = event.params.owner
-  entity.marketId = event.params.marketId
-  entity.amount = event.params.amount
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
+  let entity = Market.load(event.params.marketId.toString());
+  if (!entity) {
+    entity = new Market(event.params.marketId.toString());
+    entity.totalYes = BigInt.fromI32(0);
+    entity.totalNo = BigInt.fromI32(0);
+    entity.totalPriceToken = BigInt.fromI32(0);
+  }
+  entity.save();
 }
 
 export function handleLiquidityAdded(event: LiquidityAddedEvent): void {
-  let entity = new LiquidityAdded(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.provider = event.params.provider
-  entity.marketId = event.params.marketId
-  entity.amount = event.params.amount
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
+  let entity = Market.load(event.params.marketId.toString());
+  if (!entity) {
+    entity = new Market(event.params.marketId.toString());
+    entity.totalYes = BigInt.fromI32(0);
+    entity.totalNo = BigInt.fromI32(0);
+    entity.totalPriceToken = BigInt.fromI32(0);
+  }
+  entity.save();
 }
 
 export function handleMarketCreated(event: MarketCreatedEvent): void {
-  let entity = new MarketCreated(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.marketId = event.params.marketId
-  entity.question = event.params.question
-  entity.endTime = event.params.endTime
-  entity.creator = event.params.creator
+  let entity = new Market(event.params.marketId.toString());
+  entity.marketId = event.params.marketId;
+  entity.question = event.params.question;
+  entity.endTime = event.params.endTime;
+  entity.creator = event.params.creator;
+  entity.totalYes = BigInt.fromI32(0);
+  entity.totalNo = BigInt.fromI32(0);
+  entity.totalPriceToken = BigInt.fromI32(0);
+  entity.liquidityInitialized = true;
+  entity.claimers = [];
+  entity.resolved = false;
+  entity.createdAt = event.block.timestamp;
+  entity.updatedAt = event.block.timestamp;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
+  entity.save();
 }
 
 export function handleMarketResolved(event: MarketResolvedEvent): void {
-  let entity = new MarketResolved(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.marketId = event.params.marketId
-  entity.result = event.params.result
-  entity.totalPriceToken = event.params.totalPriceToken
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
+  let entity = Market.load(event.params.marketId.toString());
+  if (!entity) {
+    entity = new Market(event.params.marketId.toString());
+    entity.totalYes = BigInt.fromI32(0);
+    entity.totalNo = BigInt.fromI32(0);
+    entity.totalPriceToken = BigInt.fromI32(0);
+    entity.claimers = [];
+  }
+  entity.resolved = true;
+  entity.result = event.params.result;
+  entity.totalPriceToken = event.params.totalPriceToken;
+  entity.updatedAt = event.block.timestamp;
+  entity.save();
 }
 
 export function handleMarketUpdated(event: MarketUpdatedEvent): void {
-  let entity = new MarketUpdated(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.marketId = event.params.marketId
-  entity.isYesToken = event.params.isYesToken
-  entity.amount = event.params.amount
-  entity.account = event.params.account
-  entity.cost = event.params.cost
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
+  let entity = Market.load(event.params.marketId.toString());
+  if (!entity) {
+    entity = new Market(event.params.marketId.toString());
+    entity.totalYes = BigInt.fromI32(0);
+    entity.totalNo = BigInt.fromI32(0);
+    entity.totalPriceToken = BigInt.fromI32(0);
+    entity.claimers = [];
+  }
+  if (event.params.isYesToken) {
+    entity.totalYes = entity.totalYes.plus(event.params.amount);
+  } else {
+    entity.totalNo = entity.totalNo.plus(event.params.amount);
+  }
+  entity.updatedAt = event.block.timestamp;
+  entity.save();
 }
 
 export function handleOwnershipTransferred(
   event: OwnershipTransferredEvent
-): void {
-  let entity = new OwnershipTransferred(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.previousOwner = event.params.previousOwner
-  entity.newOwner = event.params.newOwner
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
+): void {}
 
 export function handleRewardClaimed(event: RewardClaimedEvent): void {
-  let entity = new RewardClaimed(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.user = event.params.user
-  entity.marketId = event.params.marketId
-  entity.rewardAmount = event.params.rewardAmount
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
+  let entity = Market.load(event.params.marketId.toString());
+  if (!entity) {
+    entity = new Market(event.params.marketId.toString());
+    entity.totalYes = BigInt.fromI32(0);
+    entity.totalNo = BigInt.fromI32(0);
+    entity.totalPriceToken = BigInt.fromI32(0);
+    entity.claimers = [];
+  }
+  entity.claimers.push(event.params.user);
+  entity.updatedAt = event.block.timestamp;
+  entity.save();
 }
 
 export function handleTokenOperation(event: TokenOperationEvent): void {
-  let entity = new TokenOperation(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.user = event.params.user
-  entity.marketId = event.params.marketId
-  entity.opType = event.params.opType
-  entity.tokenType = event.params.tokenType
-  entity.amount = event.params.amount
-  entity.cost = event.params.cost
+  let entity = Market.load(event.params.marketId.toString());
+  if (!entity) {
+    entity = new Market(event.params.marketId.toString());
+    entity.totalYes = BigInt.fromI32(0);
+    entity.totalNo = BigInt.fromI32(0);
+    entity.totalPriceToken = BigInt.fromI32(0);
+    entity.claimers = [];
+  }
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
+  if (event.params.opType === 1) {
+    if (event.params.tokenType === 1) {
+      entity.totalYes = entity.totalYes.plus(event.params.amount);
+    } else {
+      entity.totalNo = entity.totalNo.plus(event.params.amount);
+    }
+  }
+  entity.updatedAt = event.block.timestamp;
+  entity.save();
 }
